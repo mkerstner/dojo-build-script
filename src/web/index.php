7012c72463c042143bc9df1a82bcb7b0313b1e05
mkerstner <matthias@kerstner.at>
2012-12-18 22:16:46 +0100

    added comments in index.php

--- src/web/index.php
+++ src/web/index.php
@@ -2,4 +2,8 @@
 require_once("../config/base.php");
 ?>
 
-<p>Hi there, your current custom Dojo release is located at ./src/web/js/custom/release/<?php echo MY_VERSION ?></p>
+<p>Hi there, your current custom Dojo release is located at <b>./src/web/js/custom/release/<?php echo MY_VERSION ?></b></p>
+
+<p>To (re-)build your current release execute the build script from within the scripts folder.</p>
+
+<p>For more information please visit https://github.com/mkerstner/dojo-build-script/</p>
